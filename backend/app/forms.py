bb68a12c92696b8ffd0390accf5bc96cdfd1e3a6
goghtime <mbonorand@gmail.com>
2023-11-23 05:28:49 +0000

    added username login

--- backend/app/forms.py
+++ backend/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,6 +9,6 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    login = StringField('Username or Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -31,16 +31,18 @@ def register():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter((User.username == form.login.data) | (User.email == form.login.data)).first()
         if user and check_password_hash(user.password_hash, form.password.data):
             login_user(user, remember=True)
             next_page = request.args.get('next')
             return redirect(next_page or url_for('index'))
         else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
+            flash('Login Unsuccessful. Please check username, email, and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 @app.route('/logout')
 def logout():
